b982a006a299fb07950736bfdefe886c71f4a586
Mang Galang <galangpastibisa@gmail.com>
2019-12-12 16:33:03 +0800

    random by input fields

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import classes from './App.css';
+import React from 'react';
+import './App.css';
 
-class App extends Component{
-  state = {
-      value: '',
-      arrayValue: [],
-      clicked: false,
-      selectedName: ""
+const App = () => {
+  const [fields, setFields] = React.useState([]);
+  const [state, setState] = React.useState({
+    randomResult: ""
+  })
+
+  const handleChange = (i, event) => {
+    const values = [...fields];
+    values[i] = event.target.value;
+    setFields(values)
   }
 
-  handleChange = (event) => {
-    this.setState({value : event.target.value});
-    this.state.arrayValue = this.state.value.split(/[\s,]+/);
-    console.log(this.state.arrayValue);
+  const handleAdd = () => {
+    const fieldsValues = [...fields];
+    fieldsValues.push('');
+    setFields(fieldsValues);
   }
 
-  handleClick = () => {
-    this.setState({
-      clicked: true,
-      selectedName: this.state.arrayValue[Math.floor(Math.random() * this.state.arrayValue.length)]
-    });
+  const handleRemove = (i) => {
+    const fieldsValues = [...fields];
+    const showWarnig = window.confirm("Are you sure to remove this?");
+    if(showWarnig == true) {
+      fieldsValues.splice(i, 1);
+      setFields(fieldsValues);
+    }
   }
 
-  render() {
-    return(
-      <div className="App">
-        <h1>Random picker name</h1>
-        <textarea className="App__input" value={this.state.value} onChange={this.handleChange} />
-        <button onClick={this.handleClick}>Random Name</button>
-        <textarea value={this.state.selectedName} readOnly />
-      </div>
-    );
+  const handleRandom = () => {
+    const myItem = [...fields];
+    const random = myItem[Math.floor(Math.random() * myItem.length)];
+    setState({inputValue: state.inputValue, randomResult: random});
   }
+
+  return (
+    <div className="App">
+      <h1>Random picker name</h1>
+      {fields.map((field, idx) => {
+        return (
+          <div className="input-wrapper" key={`${field}-${idx}`}>
+            <input 
+              type="text"
+              value={field}
+              name="username"
+              onChange={e => handleChange(idx, e)}
+            />
+            <button onClick={() => handleRemove(idx)}>x</button>  
+          </div>
+        );
+      })}  
+      <button className="App__btn-add" onClick={handleAdd}>+</button>
+      <button className="App__btn-start" onClick={handleRandom}>Random Button</button>
+      <h2>{state.randomResult}</h2>
+    </div>
+  );
 }
 
 export default App;
